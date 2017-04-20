6553a8b1f0551c73bd3c3c03d28e2d0ea22b1c75
Fredy@Z <chizhang_2015@hotmail.com>
2017-04-20 18:28:06 -0400

    fix the rod cutting bug

--- src/main/java/edu/neu/dp/RodCutting.java
+++ src/main/java/edu/neu/dp/RodCutting.java
@@ -9,16 +9,25 @@ import java.util.List;
         {x1,x2,...,xm-1} must be the optimal solution for a rod of length n-xm
     2. Recursive solution:
        Define dp[i] to be the maximum revenue for cutting a rod of i length.
-               |- 0                      if i = 0
-       dp[i] = |- max(dp[j] + P[i-j])    for 0<=j<=i if i>0
+       dp[i] = |- 0                      if i = 0
+               |- max(dp[j] + P[i-j])    for 0<=j<=i if i>0
  */
 public class RodCutting {
+    public static void main(String[] args) {
+        RodCutting r = new RodCutting();
+        int[] prices = new int[]{1, 5, 8, 9, 10, 17, 17, 20};
+        int n = prices.length;
+        int[] dp = r.solve(prices, n);
+        System.out.println("The maximum revenue for cutting the rod is: " + dp[n]);
+        r.printRodLen(dp, prices, n);
+    }
+
     public int[] solve(int[] prices, int n) {
         int[] dp = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (i - j < prices.length) {
-                    dp[i] = Math.max(dp[j] + prices[i - j], dp[i]);
+            for (int j = 0; j < i; j++) {
+                if (i - j - 1 < prices.length) {
+                    dp[i] = Math.max(dp[j] + prices[i - j - 1], dp[i]);
                 }
             }
         }
@@ -28,25 +37,20 @@ public class RodCutting {
     private void printRodLen(int[] dp, int[] prices, int n) {
         List<Integer> rodLen = new ArrayList<>();
         int revenue = dp[n];
-        for (int i = n, j = n; i >= 0 && revenue > 0; i--) {
-            if (revenue - dp[i] != 0) {
-                rodLen.add(0, j - i);
-                revenue -= prices[j - i];
+        for (int i = n; i >= 0 && revenue > 0; ) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (revenue - prices[i - j - 1] == dp[j]) {
+                    rodLen.add(i - j);
+                    revenue -= prices[i - j - 1];
+                    i = j;
+                    break;
+                }
             }
         }
 
-        System.out.println("The cut rod lengths are: ");
+        System.out.print("The cut rod lengths are: ");
         for (Integer i : rodLen) {
             System.out.print(i + " ");
         }
-    }
-
-    public static void main(String[] args) {
-        RodCutting r = new RodCutting();
-        int n = 10;
-        int[] prices = new int[]{0, 1, 5, 8, 9, 10, 17, 18, 20, 24, 30};
-        int[] dp = r.solve(prices, n);
-        System.out.println("The maximum revenue for cutting the rod is: " + dp[n]);
-        r.printRodLen(dp, prices, n);
     }
 }
